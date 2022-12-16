e046adf4745f07278a43bfddc5cf434a988f6eeb
ba_c <swunhtetoo@gmail.com>
2022-12-16 23:08:49 +0630

    version v1 completed

--- README.md
+++ README.md
@@ -9,6 +9,8 @@
 *   A brick wall trying to query nested collections, there're just so many versions of firebase for me, each of which has super different snippets.
 *   Gonna take more time studying firebase first.
 
+* It is December 16 (wow, exactly three months) 2022. I continued on this project to wrap things up an move on to next chapters. As firebase is still confusing for me and I am now doing backend jobs myself, I just wrote this project in React. The game now works. Nothing fancy but it is functioning well. Took me around 5 hours (~16:30 to ~23:00).
+
 ## Credits
 *   svg icons downloaded from https://heroicons.com/
 *   logo is downloaded from https://www.pngegg.com/en/png-kuswz
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,46 +1,70 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Outlet, useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import data from '../assets/data';
 
 export default function Game(props) {
 
   const [content, setContent] = useState(<div>Loading...</div>)
-  const [score, setScore] = useState(0);
-  const [answerKey, setAnswerKey] = useState(['Loading']);
+  const [targetsLeft, setTargetsLeft] = useState(<div>Loading...</div>);
 
   const id = useParams().id;
 
   const already_added_answers = [];
+  let ingame = true;
+  let imgDiv, gamePhoto;
 
   const answerChecker = (arg, answers) => {
-    if(arg !== null && !already_added_answers.includes(arg)){
+
+    if(ingame && arg !== null && !already_added_answers.includes(arg)){
+
       already_added_answers.push(arg);
-      setScore(answers.length - already_added_answers.length);
+      setTargetsLeft(answers.length - already_added_answers.length);
+
+      const clientRect = gamePhoto.getBoundingClientRect();
+      drawCircle(
+        imgDiv,
+        arg.x*clientRect.width/100,
+        arg.y*clientRect.height/100,
+        true);
+ 
+      if(answers.length - already_added_answers.length === 0) ingame = false;
     }
+
+
   }
 
   const photoClicked = (e, answers) => {
 
-    const gamePhoto = document.getElementById('game_photo');
     const clientRect = gamePhoto.getBoundingClientRect();
   
     const x = Math.round(((e.clientX-clientRect.left)/clientRect.width)*100);
     const y = Math.round(((e.clientY-clientRect.top)/clientRect.height)*100);
 
-    console.log(answers)
+    let isCorrect = false;
   
     answers.forEach(
       xy => {
-        if( x >= xy.x-5 && x <= xy.x+5 
-          && y >= xy.y-5 && y <= xy.y+5){
-            answerChecker(xy, answers)
-            return;
+        if(  Math.abs(xy.x - x) <= 3
+          && Math.abs(xy.y - y) <= 3  ){
+            answerChecker(xy, answers);
+            isCorrect = true;
           }
       }
     )
-    answerChecker(null, answers)
+
+    if(!isCorrect){
+      drawCircle(
+        imgDiv,
+        e.clientX-clientRect.left,
+        e.clientY-clientRect.top,
+        false);
+  
+      answerChecker(null, answers)
+    }
+
+    
   }
   
 
@@ -49,7 +73,7 @@ export default function Game(props) {
 
       const sampleImg = data.sample;
       const card = data.questions.filter(q => q.id == id)[0];
-      setAnswerKey(card.answers);
+      setTargetsLeft(card.answers.length);
 
       setContent(
         <div className="grid grid-cols-6 grid-rows-1 p-5 gap-5 md:w-9/12 m-auto">
@@ -58,12 +82,20 @@ export default function Game(props) {
             <p className='mt-3 text-blue-800 text-center ml-auto'>5/5</p>
           </div>
           <div className='col-span-5'>
-            <div className="rounded-lg drop-shadow">
+            <div className="rounded-lg drop-shadow relative">
+              <div id='img_area' className='w-full h-full bg-yellow-100 absolute'>
+              </div>
               <img src={card.img} alt="" id='game_photo' className='rounded-lg drop-shadow' onClick={ (e) => photoClicked(e, card.answers) }/>
             </div>
           </div>
         </div>
       )
+
+      setTimeout(() => {
+        imgDiv = document.getElementById('img_area');
+        gamePhoto = document.getElementById('game_photo');
+
+      }, 10);
     }, []
   )
 
@@ -73,8 +105,28 @@ export default function Game(props) {
       
             {content}
 
-            <p className='text-center'>Completed {score} </p>
+            {targetsLeft > 0 && <p className='text-center'> {targetsLeft} left </p>}
+
+            {targetsLeft === 0 && <p className='text-blue-800 text-center'> Congratualations. </p>}
         
     </div>
   );
+}
+
+function drawCircle(parent, x, y, isCorrect){
+  const e = document.createElement('div');
+  e.style.left = (x-20) + 'px';
+  e.style.top = (y-20) + 'px';
+
+  if(isCorrect){
+    e.className = `bg-blue-500 z-10 rounded-full absolute w-10 h-10 border-black border-dashed border-2`;
+    e.style.opacity = '0.6';
+
+  } else {
+    e.className = `bg-red-500 z-10 rounded-full absolute w-10 h-10 border-black border-dashed border-2 ease-in-out duration-500`;
+    setTimeout(
+      () => { e.style.opacity = 0 }, 100
+    )
+  }
+  parent.append(e);
 }
